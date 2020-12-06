aa59ceda4e7ceba3b9f997f1efe991eaa20968c4
Szilard Nemeth <szilard.nemeth88@gmail.com>
2020-12-06 18:23:16 +0100

    Initial commit

---
+++ src/main/java/com/szilardnemeth/evernote/playground/EvernotePlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.szilardnemeth.evernote.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvernotePlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvernotePlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/szilardnemeth/evernote/playground/EvernotePlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szilardnemeth.evernote.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvernotePlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
